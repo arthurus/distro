65797756ad72b7c8f10db75b339aa3441d2915eb
Artur Paszkiewicz <artur.paszkiewicz@gmail.com>
2016-06-08 22:11:18 +0200

    dhcpcd: changed paths

--- packages/dhcpcd/build.sh
+++ packages/dhcpcd/build.sh
@@ -1,7 +1,7 @@
 SRC_TAR_URL="http://roy.marples.name/downloads/dhcpcd/dhcpcd-6.11.0.tar.xz"
 
 prepare () {
-	./configure --host=$TARGET --build=x86_64-pc-linux-gnu --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+	./configure --host=$TARGET --build=x86_64-pc-linux-gnu --prefix=/usr --bindir=/bin --sbindir=/sbin --sysconfigdir=/etc --libdir=/lib --localstatedir=/var --libexecdir=/lib/dhcpcd
 }
 
 build () {
@@ -9,5 +9,6 @@ build () {
 }
 
 install () {
-	install_to_sysroot
+	install_to_sysroot || return 1
+	sudo cp -r "$PKG_DIR/services" "$SYSROOT/lib/"
 }
