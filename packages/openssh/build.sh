2ef207c73aed249dd82bc5e7e514fbe10ec4ac44
Artur Paszkiewicz <artur.paszkiewicz@gmail.com>
2016-06-15 23:14:04 +0200

    openssh startup script and configs

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -2,7 +2,7 @@ SRC_TAR_URL="http://ftp.icm.edu.pl/pub/OpenBSD/OpenSSH/portable/openssh-7.2p2.ta
 
 prepare () {
 	patch -i $PKG_DIR/Makefile.in.patch
-	./configure --host=$TARGET --build=x86_64-pc-linux-gnu --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-lastlog
+	./configure --host=$TARGET --build=x86_64-pc-linux-gnu --prefix=/usr --sysconfdir=/etc/ssh --localstatedir=/var --disable-lastlog
 }
 
 build () {
@@ -13,4 +13,7 @@ install () {
 	install_to_sysroot || return 1
 	sudo chown root:root $SYSROOT/var/empty || return 1
 	sudo chmod 755 $SYSROOT/var/empty || return 1
+	sudo install -m 754 $PKG_DIR/sshd $SYSROOT/etc/init.d/ || return 1
+	sudo sed -i.bak 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' $SYSROOT/etc/ssh/sshd_config || return 1
+	sudo rm $SYSROOT/etc/ssh/sshd_config.bak
 }
