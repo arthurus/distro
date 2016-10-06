2d16c33057205779a110a03204574b7607c5ebae
Artur Paszkiewicz <artur.paszkiewicz@gmail.com>
2016-10-06 23:19:00 +0200

    openssh: enable sshd on startup

--- packages/openssh/package.sh
+++ packages/openssh/package.sh
@@ -1,7 +1,7 @@
 SRC_TAR_URL="http://ftp.icm.edu.pl/pub/OpenBSD/OpenSSH/portable/openssh-7.2p2.tar.gz"
 
 prepare () {
-	patch -i $PKG_DIR/Makefile.in.patch
+	patch -i $PKG_DIR/Makefile.in.patch || return 1
 	./configure --host=$TARGET --build=x86_64-pc-linux-gnu --prefix=/usr --sysconfdir=/etc/ssh --localstatedir=/var --disable-lastlog
 }
 
@@ -15,5 +15,6 @@ install () {
 	sudo chmod 755 $SYSROOT/var/empty || return 1
 	sudo install -m 754 $PKG_DIR/sshd $SYSROOT/etc/init.d/ || return 1
 	sudo sed -i.bak 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' $SYSROOT/etc/ssh/sshd_config || return 1
-	sudo rm $SYSROOT/etc/ssh/sshd_config.bak
+	sudo rm $SYSROOT/etc/ssh/sshd_config.bak || return 1
+	sudo $HOSTTOOLS_DIR/insserv -c $HOSTTOOLS_DIR/insserv.conf -p $SYSROOT/etc/init.d $SYSROOT/etc/init.d/sshd
 }
